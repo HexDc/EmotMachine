d040140192d85985916a65efc43f58b63355994b
HexDc <41858208+HexDc@users.noreply.github.com>
2019-05-08 13:56:13 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,33 @@
-class
+import os
+from discord.ext import commands
+import discord
+import json, asyncio
+import configparser
+import traceback
+import sys
+import os
+import re
+import ast
+import shutil
+from pathlib import Path
+
+token = 'bot token'
+
+prefix = "!"
+
+description = !list 
+
+
+
+bot = commands.Bot(command_prefix=prefix, description=description)
+
+@bot.command(hidden=True)
+async def list(ctx):
+
+    await (await bot.ws.ping())
+
+    now = datetime.datetime.now()
+
+    return now - msgtime
+  
+bot.run(token)
